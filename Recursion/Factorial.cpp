6d9d52d22488694dadb6b1e184db2a987c864912
Ahmedsabib <67859165+Ahmedsabib@users.noreply.github.com>
2020-12-03 19:02:41 +0600

    Create Factorial.cpp

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fact(int x){
+ if(x >= 1){
+  return x*fact(x-1);
+ }
+ else return 1;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  cout << fact(n) << '\n';
+  return 0;
+}
