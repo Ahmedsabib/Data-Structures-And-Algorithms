7b71fccd09bf4d68d8cbcc322dcad31f21850531
Ahmedsabib <67859165+Ahmedsabib@users.noreply.github.com>
2021-04-26 21:27:15 +0600

    Create CPP STL Set.cpp

---
+++ CPP STL/CPP STL Set.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+int main() {
+	using namespace std;
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	std::set<int>S = {1, 2, 3, 4, 5, 6}; // every element is present with only one number of time and elements are sorted automatically
+
+	/**
+	* Looping Throug a Set
+	**/
+	for(auto it = S.begin(); it != S.end(); ++it){
+		std::cout << *it << ' ';
+	}
+
+	/**
+	* std::set<Key,Compare,Allocator>::empty
+	**/
+	//check if the set is empty or not
+	if(S.empty()) std::cout << "YES" << '\n';
+	else std::cout << "NO" << '\n';
+
+	/**
+	* std::set<Key,Compare,Allocator>::size
+	**/
+	//output thew size of the set
+	std::cout << S.size() << '\n';
+
+	/**
+	* std::set<Key,Compare,Allocator>::insert
+	**/
+	//inserts an element to the set
+	S.insert(9);
+	std::cout << S.size() << '\n';
+
+	/**
+	* std::set<Key,Compare,Allocator>::erase
+	**/
+	//erase a specific element
+	for(auto x: S){
+		std::cout << x << ' ';
+	}
+	std::cout << '\n';
+
+	/**
+	* std::set<Key,Compare,Allocator>::count
+	**/
+	//output the number of times the element occured in the set
+	std::cout << S.count(5) << '\n';
+
+	/**
+	* std::set<Key,Compare,Allocator>::find
+	**/
+	//search for an element in the set
+	auto it = S.find(2);
+	if(it != S.end()){
+		std::cout << "Found" << ' ' << *it << '\n'; 
+	} else{
+		std::cout << "Not Found" << '\n';
+	}
+
+	/**
+	* std::set<Key,Compare,Allocator>::contains [This is a C++20 function, so it won't work in the compilers below that and will get an error]
+	**/
+	//search for an element in the set and check if it contains that element
+	for(auto x: S){
+		if(S.contains(4)) std::cout << "Found" << '\n';
+		else std::cout << "Not Found" << '\n';
+	}
+
+	/**
+	* std::set<Key,Compare,Allocator>::clear
+	**/
+	//clear all elements and make the set empty
+	S.clear();
+	std::cout << S.size() << '\n';
+
+
+	return 0;
+}
